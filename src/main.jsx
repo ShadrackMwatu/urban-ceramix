100803594f572586ee55d9fb441151b8a0754422
ShadrackMwatu <129580337+ShadrackMwatu@users.noreply.github.com>
2025-07-31 01:28:58 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
-import React from "react"; import ReactDOM from "react-dom/client"; import UrbanCeramiX from "./UrbanCeramiX"; ReactDOM.createRoot(document.getElementById("root")).render(<UrbanCeramiX />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UrbanCeramiX from "./UrbanCeramiX";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UrbanCeramiX />
+  </React.StrictMode>
+);
